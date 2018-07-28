3336b61c812b37ee5d0e8e4f4709663638a4ee00
smilecs <mumene@gmail.com>
2018-07-28 16:28:36 +0100

    heroku

--- src/main/kotlin/config/DbClient.kt
+++ src/main/kotlin/config/DbClient.kt
@@ -8,9 +8,10 @@ import org.litote.kmongo.getCollection
 
 object DbClient {
     const val BoardModelCol = "BoardModelCol"
+    val url:String = if(System.getenv("MONGOLAB_URI").isNotEmpty()) System.getenv("JDBC_DATABASE_URL") else ""
 
     fun BoardModelCol(): MongoCollection<UserBoard> {
-        return KMongo.createClient().run {
+        return KMongo.createClient(url).run {
             getDatabase(BoardModelCol).getCollection()
         }
     }
